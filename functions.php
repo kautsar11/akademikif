458b42c36efe29787a78635dc782da851b235c79
kautsar <kautsarteguh2002@gmail.com>
2022-07-25 18:38:00 +0700

    menambahkan file functions.php

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+$conn = connect_to_database();
+
+function getRowDataGuru($nip)
+{
+    global $conn;
+    $stmt = $conn->prepare("SELECT * FROM guru WHERE nip = ?");
+    $stmt->bind_param("s", $nip);
+    $stmt->execute();
+    $result = $stmt->get_result()->fetch_assoc();
+
+    return $result;
+}
